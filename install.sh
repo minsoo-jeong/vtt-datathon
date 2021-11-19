667e3abca1ff5a4ef25f2786b8a51434dfb959dc
ms-jeong <jms8167@gmail.com>
2021-11-19 17:26:52 +0900

    .

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 python -m pip install -e mish-cuda
 pip install -r requirements.txt
 apt install libgl1-mesa-glx libglib2.0-0 -y
-wget https://github.com/ms-jeong/vtt-datathon/releases/download/v1/best.pt ./ScaledYOLOv4/best.pt
+cd ScaledYOLOv4
+wget https://github.com/ms-jeong/vtt-datathon/releases/download/v1/best.pt ./best.pt
